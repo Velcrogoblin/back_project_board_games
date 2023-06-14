b54937bf971dbae4f74a0e44b9589d615a4e857e
Erik <mortensenerikm@gmail.com>
2023-06-14 18:01:15 -0300

    Renamed ids in models

--- src/models/Mechanic.js
+++ src/models/Mechanic.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("mechanic", {
-    id: {
+    mechanic_id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true
