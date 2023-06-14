656360017bddd0f73c7499449dfc1c3b467f475e
nahuel <lescanoagustin10nahuel@gmail.com>
2023-06-13 22:25:38 -0300

    Elimino la relacion de dog.

--- src/db.js
+++ src/db.js
@@ -38,7 +38,6 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog, Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews)
