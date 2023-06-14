0fb08606967d8cf84cd8ea57e2b9f559a5596603
nahuel <lescanoagustin10nahuel@gmail.com>
2023-06-13 22:24:29 -0300

    Nico acepta el pull.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const { PORT } = process.env;
+
+conn.sync({ force: true }).then(() => {
+	console.log("Hacepta el pull loco");
+	console.log("Connecting to the database");
+	server.listen(PORT, () => {
+		console.log(`Listening at port: ${PORT}`);
+	});
+});
