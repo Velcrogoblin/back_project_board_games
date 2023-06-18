4212bdeeb845af14d27f0eb3a04553bc97b9aede
Erik <mortensenerikm@gmail.com>
2023-06-18 01:43:50 -0300

    getAllCategories & createCategory created

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,13 @@ const { Router } = require("express");
 const EditorialRoutes = require("./EditorialRoutes.js");
 const LanguageRoutes = require("./LanguageRoutes.js");
 const gameRoutes = require("./gameRoutes.js");
+const categoriesRoutes = require("./categoriesRoutes.js");
+
 const router = Router();
 
 router.use("/Editorials", EditorialRoutes);
 router.use("/Languages", LanguageRoutes);
 router.use("/games", gameRoutes);
+router.use("/categories", categoriesRoutes);
 
 module.exports = router;
