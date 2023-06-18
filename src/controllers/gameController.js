035024d89af23f29068a541ca9b4968df55c4101
unknown <ddandolo@yahoo.com>
2023-06-18 13:21:02 -0300

    borre ruta de dogs

--- src/controllers/gameController.js
+++
@@ -1,7 +0,0 @@
-const getAllGames = (req, res) => {
-  return res.status(200).json({ message: "Dale Nico acepta el pull." });
-};
-
-module.exports = {
-  getAllGames,
-};
--- src/routes/dogRoute.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { getAllGames } = require("../controllers/gameController");
-
-const router = Router();
-
-router.get("/", getAllGames);
-
-module.exports = router;
