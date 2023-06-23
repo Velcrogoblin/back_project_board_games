8b0d5c84e35f52c3e853fa6928bc8af160d3032a
Erik <mortensenerikm@gmail.com>
2023-06-23 01:49:15 -0300

    Put & Delete for Editorial, Language, Category and Mechanic

--- api/src/controllers/categoryController.js
+++ api/src/controllers/categoryController.js
@@ -39,7 +39,46 @@ const getAllCategories = async (req, res) => {
   }
 };
 
+const deleteCategory = async (req, res) => {
+  const { id } = req.params;
+
+  if (isNaN(id)) return res.status(400).json({ message: 'The id must be a number.' });
+
+  try {
+    const category = await Category.findByPk(id);
+    if (category) {
+      category.active = false;
+      await category.save();
+      return res.status(200).json({ message: `The category ${category.category_name} has been deleted.` });
+    }
+    res.status(404).json({ message: `There is no category with id ${id}.` });
+  } catch ({ message }) {
+    res.status(500).json({ error: message });
+  }
+}
+
+const putCategory = async (req, res) => {
+  const { category_id, category_name, active } = req.body;
+
+  if (!category_id) return res.status(400).json({ message: 'Id is required.' });
+
+  try {
+    const category = await Category.findByPk(category_id);
+    if (category) {
+      if (category_name) category.category_name = category_name;
+      if (active !== undefined) category.active = active;
+      await category.save();
+      return res.status(200).json({ message: `The category has been updated.` });
+    }
+    res.status(404).json({ message: `There is no category with id ${category_id}.` });
+  } catch ({ message }) {
+    res.status(500).json({ error: message });
+  }
+}
+
 module.exports = {
   postCategory,
   getAllCategories,
+  deleteCategory,
+  putCategory
 };
--- api/src/routes/EditorialRoutes.js
+++ api/src/routes/EditorialRoutes.js
@@ -2,9 +2,14 @@ const { Router } = require("express");
 const {
   getAllEditorials,
   postEditorial,
+  deleteEditorial,
+  putEditorial,
 } = require("../controllers/editorialController");
 const router = Router();
 
-router.get("/", getAllEditorials).post("/", postEditorial);
+router.get("/", getAllEditorials)
+  .post("/", postEditorial)
+  .delete("/:id", deleteEditorial)
+  .put("/", putEditorial);
 
 module.exports = router;
