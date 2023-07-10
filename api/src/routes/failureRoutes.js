b59081492e4d51e29e80ddc9fc54d8a74cdee0fe
nahuel <lescanoagustin10nahuel@gmail.com>
2023-07-10 19:20:32 -0300

    Trying to solve mercadopago issues.

--- api/src/routes/failureRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const { failure } = require("../controllers/statePayment");
-
-const router = Router();
-
-router.get("/", failure);
-
-module.exports = router;
--- api/src/routes/webhookRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const { receiveWebhook } = require("../controllers/webhookController");
-
-const router = Router();
-
-router.post("/", receiveWebhook);
-
-module.exports = router;
