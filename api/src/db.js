7b7f7d8d000f6a464561ef6404f1e26479ddbf5d
Erik Mortensen <mortensenerikm@gmail.com>
2023-07-12 16:43:51 -0300

    para cambiar de rama

--- api/src/db.js
+++ api/src/db.js
@@ -5,8 +5,8 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
 const sequelize = new Sequelize(
-  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  DB_DEPLOY,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  // DB_DEPLOY,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
