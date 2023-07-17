42c0ac5335fdf5effabc013f0bc0c5359ec123f4
nahuel <lescanoagustin10nahuel@gmail.com>
2023-07-16 22:46:30 -0300

    Searching by user id and purchase id.

--- api/src/controllers/purchaseController.js
+++ api/src/controllers/purchaseController.js
@@ -1,4 +1,4 @@
-const { Purchase, User, Game } = require("../db");
+const { Purchase, User, Game, Op } = require("../db");
 
 const getAllPurchase = async (req, res) => {
   try {
@@ -11,6 +11,62 @@ const getAllPurchase = async (req, res) => {
     }
 
     return res.status(200).json(purchases);
+  } catch ({ message }) {
+    return res.status(500).json({ message });
+  }
+};
+
+const getPurchaseByIdUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (id === "") {
+      return res.status(400).json({ message: "id is not valid." });
+    }
+
+    const userPurchases = await Purchase.findAll({
+      where: {
+        user_id: {
+          [Op.iLike]: `%${id}%`,
+        },
+      },
+
+      include: [{ model: User }],
+    });
+
+    if (!userPurchases) {
+      return res
+        .status(400)
+        .json({ message: `No purchases with user id ${id} was found.` });
+    }
+
+    return res.status(200).json(userPurchases);
+  } catch ({ message }) {
+    return res.status(500).json({ message });
+  }
+};
+
+const getPurchaseById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (isNaN(id)) {
+      return res.status(400).json({ message: "id is not valid." });
+    }
+
+    const existingPurchase = await Purchase.findOne({
+      where: {
+        purchase_id: {
+          [Op.eq]: id,
+        },
+      },
+      include: [{ model: User }],
+    });
+    if (!existingPurchase) {
+      return res
+        .status(400)
+        .json({ message: `No purchase with id ${id} was found.` });
+    }
+
+    return res.status(200).json(existingPurchase);
   } catch ({ message }) {
     return res.status(500).json({ message });
   }
@@ -103,6 +159,8 @@ const deletePurchase = async (req, res) => {
 
 module.exports = {
   getAllPurchase,
+  getPurchaseByIdUser,
+  getPurchaseById,
   postPurchase,
   deletePurchase,
 };
