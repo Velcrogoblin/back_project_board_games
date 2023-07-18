4f2004472f07d7ed8df362341a1cb5db1320123c
Erik Mortensen <mortensenerikm@gmail.com>
2023-07-18 00:16:13 -0300

    firebase en el back

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getUsers, getUserById, createUser, deleteUser, putUser, addShippingAddress, getShippingAddressById } = require("../controllers/userController");
+const { getUsers, getUserById, createUser, deleteUser, putUser, addShippingAddress, getShippingAddressById, verifyEmail } = require("../controllers/userController");
 
 const router = Router();
 
@@ -9,6 +9,7 @@ router.get("/", getUsers)
   .post("/", createUser)
   .post("/shippingaddress", addShippingAddress)
   .delete("/:uid", deleteUser)
-  .put("/", putUser);
+  .put("/", putUser)
+  .put("/verifyemail/:id", verifyEmail);
 
 module.exports = router;
